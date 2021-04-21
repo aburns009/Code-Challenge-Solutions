f0e164b6d20133b9f1919b437268cfcfd53233b6
aburns009 <aburns009@gmail.com>
2021-04-21 15:14:20 -0700

    sum ints not included in an arr from min to max

--- Challenges.js
+++ Challenges.js
@@ -96,3 +96,21 @@ const firstRepeat = chars => {
   const dub = chars.match(/(\w)(?=.*\1)/);
   return dub != null ? dub[0] : '-1';
 };
+
+//Edabit: sum the numbers not included in an arr from min to max
+const sumMissingNumbers = arr => {
+  let sum = 0;
+  const s = arr.sort((a, b) => a - b);
+  for (let i = arr[0]; i < s[s.length - 1]; i++) {
+    if (!s.includes(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+/*Best Solution: const sumMissingNumbers = a => {
+  let tot = 0;
+  for (let i = Math.min(...a) + 1, j = Math.max(...a); i < j; i++)
+    if (!a.includes(i)) tot += i;
+  return tot;
+};*/
--- Practice Env.js
+++ Practice Env.js
@@ -1,5 +1,16 @@
-const firstRepeat = chars => {
-  const dub = chars.match(/(\w)(?=.*\1)/);
-  return dub != null ? dub[0] : '-1';
+const sumMissingNumbers = arr => {
+  let sum = 0;
+  const s = arr.sort((a, b) => a - b);
+  for (let i = arr[0]; i < s[s.length - 1]; i++) {
+    if (!s.includes(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 };
-firstRepeat('Galadriel');
+const sumMissingNumbers = a => {
+  let tot = 0;
+  for (let i = Math.min(...a) + 1, j = Math.max(...a); i < j; i++)
+    if (!a.includes(i)) tot += i;
+  return tot;
+};
