5ad9a269e4017b3884ba7221a290baa07e1dd74b
aburns009 <aburns009@gmail.com>
2021-07-07 15:39:53 -0700

    halflife calculator

--- Challenges.js
+++ Challenges.js
@@ -355,3 +355,13 @@ const likeOrDislike = arr => {
 //Best Solution:
 const likeOrDislike = arr =>
   arr.reduce((a, b) => (a === b ? 'Nothing' : b), 'Nothing');
+
+//Edabit: halflife calculator
+const halfLifeCalculator = (mass, hlife, n) => {
+  let count = n;
+  while (count > 0) {
+    mass = mass / 2;
+    count--;
+  }
+  return [+mass.toFixed(3), hlife * n];
+};
--- Practice Env.js
+++ Practice Env.js
@@ -1,10 +1,8 @@
-const evenOrOdd = str => {
-  let odd = 0;
-  let even = 0;
-  [...str].map(x => (x % 2 != 0 ? (odd += +x) : (even += +x)));
-  if (odd > even) {
-    return 'Odd is greater than Even';
-  } else if (even > odd) {
-    return 'Even is greater than Odd';
-  } else return 'Even and Odd are the same';
+const halfLifeCalculator = (mass, hlife, n) => {
+  let count = n;
+  while (count > 0) {
+    mass = mass / 2;
+    count--;
+  }
+  return [+mass.toFixed(3), hlife * n];
 };
