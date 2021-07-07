e15bb9873a453c1aaa6d514c7ca0a1f12dae8d07
aburns009 <aburns009@gmail.com>
2021-07-07 16:21:06 -0700

    dna to mRNA converter

--- Practice Env.js
+++ Practice Env.js
@@ -1,8 +1,1 @@
-const halfLifeCalculator = (mass, hlife, n) => {
-  let count = n;
-  while (count > 0) {
-    mass = mass / 2;
-    count--;
-  }
-  return [+mass.toFixed(3), hlife * n];
-};
+const findZip = str => str.indexOf('zip', str.indexOf('zip' + 1));
